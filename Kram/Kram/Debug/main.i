ee42723ff3099ec3c86e2e3f07260de45244f570
Marc Pascual Martinez <marc.pascual@tmtfactory.com>
2019-05-27 11:24:20 +0200

    Reset project

--- Kram/Kram/Debug/main.i
+++
@@ -1,7 +0,0 @@
-#line 1 "d:\\kram\\kram\\kram\\main.cpp"
-
-
-int main(int argc, char** argv)
-{
-	return 0;
-}
--- Kram/Kram/main.cpp
+++ Kram/Kram/main.cpp
@@ -1,17 +1,8 @@
-#include "types.h"
 #include <iostream>
 
 int main(int argc, char** argv)
 {
-	kint8 val = 10;
 
-	kint8 other_val = val;
-	kint8 last(0);
-	last = val;
-
-	std::cout << "val: " << &val << "; other_val: " << &other_val << "; last: " << &last << std::endl;
-
-	std::cout << sizeof(kint8) << std::endl;
 
 	std::system("pause");
 
